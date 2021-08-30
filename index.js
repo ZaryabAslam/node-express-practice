93e2e473ffbc68f9cb86dff5ee0a8e25cd138ef2
Zaryab Aslam <zaryab.aslam@systemsltd.com>
2021-08-30 14:42:57 +0500

    weather api-live

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require('cors')
-const mongoose = require('mongoose');
-const authModel = require('./models/authSchema')
-const SignupModel = require('./models/signup');
-const bcrypt = require('bcryptjs');
-const mainRoute = require('./route/mainRoute')
-const PORT = 8080;
+const https = require('https')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 5000;
+const celcius= 'units=metric'
+
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-mongoose.connect('mongodb+srv://admin:passwordpassword@cluster0.ajxov.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-app.use(mainRoute)
+app.use(bodyParser.json())
 
-mongoose.connection.on('connected', () => {
-    console.log('mongodb connected to node')
-})
-mongoose.connection.on('error', () => {
-    console.log('mongodb not connected to node')
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+app.post('/submit', (req,res)=>{
+    const city = req.body.city
+    const API_KEY = '34174d5ed9074754a51bf62a23e170f6';
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&${celcius}`
+    https.get(URL, response=>{
+        response.on('data', (chunk) => {
+          var weatherInfo = JSON.parse(chunk)
+          var clouds = weatherInfo.weather[0].main
+          var temp = weatherInfo.main.temp
+          res.write(`<h1>The Weather is ${clouds} and the temp in ${city} is ${temp}</h1>`)
+          res.send()
+        });
+      })
+} )
 
-
-app.listen(PORT, () => {
+app.listen(PORT, ()=>{
     console.log(`server running on ${PORT}`)
 })
